6ac0495e4d7972413b6958bcf707be7b18ac861e
Adeku Abdulbaasit Mayokun <105329609+maycodin@users.noreply.github.com>
2023-09-08 04:22:01 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+function displayTime() {
+    const WEEK = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+    const Time = document.getElementById('time');
+    const Day = document.getElementById('day');
+
+    const UTCTime = new Date().getTime();
+    const dayOfWeekNumber = new Date().getDay();
+    const dayOfWeek = WEEK[dayOfWeekNumber] 
+
+    Time.textContent = `Current UTC Time in milliseconds: ${UTCTime}`;
+    Day.textContent = `Current Day of The Week: ${dayOfWeek}`;
+}
+displayTime()
